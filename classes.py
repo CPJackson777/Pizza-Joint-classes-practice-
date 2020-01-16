81af80481813f174279a68a9b9aad28ad16e271a
Charles Jackson <cpjackson572@gmail.com>
2020-01-16 14:34:59 -0600

    created pizza.py

--- classes.py
+++ classes.py
@@ -4,17 +4,6 @@
 
 # Add a method for outputting a description of the pizza (sample output below).
 
-# Make two different instances of a pizza. If you have properly defined the class, you should be able to do something like the following code with your Pizza type.
-
-# meat_lovers = Pizza()
-# meat_lovers.size = 16
-# meat_lovers.style = "Deep dish"
-# meat_lovers.add_topping("Pepperoni")
-# meat_lovers.add_topping("Olives")
-# meat_lovers.print_order()
-# You should produce output in the terminal similiar to the following string.
-
-# "I would like a 16-inch, deep-dish pizza with Pepperoni and Olives."
 
 class Pizza:
     def __init__(self, size, crust):
